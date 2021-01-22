215fa988738b7b8fe7061ab0f8f0421fe7ae4845
Reo98105 <gundamzone00@gmail.com>
2021-01-23 01:23:45 +0800

    Added some lines to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,18 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp();
+void main() => runApp(
+      MaterialApp(
+        home: Home(),
+      ),
+    );
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+    );
+  }
 }
