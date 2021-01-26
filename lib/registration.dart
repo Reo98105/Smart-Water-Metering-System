8f5a3eb259e674a5033081fc0eaa78d752329345
Reo98105 <gundamzone00@gmail.com>
2021-01-27 01:21:17 +0800

    Added validation feature

--- lib/registration.dart
+++ lib/registration.dart
@@ -1,22 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:swms_user_auth_module/login.dart';
 
-class Register extends StatefulWidget {
-  @override
-  _RegisterState createState() => _RegisterState();
-}
-
-class _RegisterState extends State<Register> {
-  bool isCheck = false;
-
+class Register extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomPadding: false,
       appBar: AppBar(
         title: Text('Register'),
         backgroundColor: Colors.lightBlueAccent,
       ),
-      body: Container(
+      body: RegisterForm(),
+    );
+  }
+}
+
+class RegisterForm extends StatefulWidget {
+  @override
+  _RegisterFormState createState() => _RegisterFormState();
+}
+
+class _RegisterFormState extends State<RegisterForm> {
+  bool isCheck = false;
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Container(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -24,6 +37,12 @@ class _RegisterState extends State<Register> {
             Container(
               margin: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
               child: TextFormField(
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter Username';
+                  }
+                  return null;
+                },
                 decoration: InputDecoration(
                   labelText: 'Username',
                 ),
@@ -32,25 +51,49 @@ class _RegisterState extends State<Register> {
             Container(
               margin: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
               child: TextFormField(
+                obscureText: true,
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter Password';
+                  }
+                  return null;
+                },
                 decoration: InputDecoration(
                   labelText: 'Password',
+                  suffixIcon: Icon(Icons.lock),
                 ),
               ),
             ),
             Container(
               margin: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
               child: TextFormField(
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter Email';
+                  }
+                  return null;
+                },
                 decoration: InputDecoration(
                   labelText: 'Email',
                 ),
               ),
             ),
             Container(
-              margin: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+              margin: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 10.0),
               child: TextFormField(
+                keyboardType: TextInputType.number,
+                inputFormatters: [
+                  FilteringTextInputFormatter.allow(RegExp('[0-9]'))
+                ],
                 decoration: InputDecoration(
                   labelText: 'NRIC',
                 ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter NRIC';
+                  }
+                  return null;
+                },
               ),
             ),
             Container(
@@ -59,9 +102,15 @@ class _RegisterState extends State<Register> {
                 children: <Widget>[
                   Container(
                     margin:
-                        EdgeInsets.symmetric(horizontal: 5.0, vertical: 10.0),
+                        EdgeInsets.symmetric(horizontal: 5.0, vertical: 15.0),
                     child: ElevatedButton.icon(
-                      onPressed: isCheck ? () {} : null,
+                      onPressed: isCheck
+                          ? () {
+                              if (_formKey.currentState.validate())
+                                debugPrint(
+                                    'Posting to server for registration...');
+                            }
+                          : null,
                       icon: Icon(Icons.app_registration),
                       label: Text('Register'),
                       style: ElevatedButton.styleFrom(
@@ -73,7 +122,7 @@ class _RegisterState extends State<Register> {
                   ),
                   Container(
                     margin:
-                        EdgeInsets.symmetric(horizontal: 5.0, vertical: 10.0),
+                        EdgeInsets.symmetric(horizontal: 5.0, vertical: 15.0),
                     child: ElevatedButton.icon(
                       onPressed: () {
                         Navigator.pop(
@@ -96,7 +145,7 @@ class _RegisterState extends State<Register> {
               ),
             ),
             Container(
-              margin: EdgeInsets.fromLTRB(5.0, 50.0, 5.0, 5.0),
+              margin: EdgeInsets.fromLTRB(5.0, 100.0, 5.0, 5.0),
               child: CheckboxListTile(
                 value: isCheck,
                 onChanged: (bool value) {
@@ -108,7 +157,7 @@ class _RegisterState extends State<Register> {
                 },
                 title: Text(
                   'By clicking Register, you agree to our Terms and that you have read our Data Use Policy, including our Cookie Use.',
-                  style: TextStyle(fontSize: 13.0),
+                  style: TextStyle(fontSize: 12.0),
                 ),
                 controlAffinity: ListTileControlAffinity.leading,
                 activeColor: Colors.lightBlueAccent,
