e18abd94e34e98444513ff60051ec8cefec0f660
Reo98105 <gundamzone00@gmail.com>
2021-01-28 19:16:41 +0800

    Added DAO, DBConnection and splashscreen

---
+++ lib/DAO/registerDAO.dart
@@ -0,0 +1,5 @@
+import 'package:swms_user_auth_module/DBConnection/mysql.dart';
+
+class RegisDAO{
+  var conn = new Mysql();
+}
---
+++ lib/DBConnection/mysql.dart
@@ -0,0 +1,18 @@
+import 'package:mysql1/mysql1.dart';
+
+class Mysql {
+  static String host = "10.0.2.2",
+      user = "root",
+      password = "admin",
+      db = "testdb";
+
+  static int port = 3306;
+
+  Mysql();
+
+  Future<MySqlConnection> getConnection() async {
+    var settings = new ConnectionSettings(
+        host: host, port: port, user: user, password: password, db: db);
+    return await MySqlConnection.connect(settings);
+  }
+}
